fdf92d9c42b2f664ab85bc67f586624442780b96
radha krishna valmiki <radhakrishnavalmiki@radhas-MBP.attlocal.net>
2018-03-26 23:21:59 -0500

    first commit

---
+++ examples/peoplemanagementspringbootdataapp/src/main/java/com/codem/people/app/peoplemanagementspringbootdataapp/PeoplemanagementspringbootdataappApplication.java
@@ -0,0 +1,12 @@
+package com.codem.people.app.peoplemanagementspringbootdataapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplemanagementspringbootdataappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplemanagementspringbootdataappApplication.class, args);
+	}
+}
